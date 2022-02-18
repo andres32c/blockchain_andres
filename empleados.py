8766e1b5bfecac3744316baa607fbca541ac1857
andres carvajal <andres.carvajal@arroyotraining.net>
2022-02-17 19:48:04 -0500

    nueva entrada

---
+++ empleados.py
@@ -0,0 +1,23 @@
+empleado= [["juan",(2000,3000,4233)] , ["ana",(3444,1000,5333)]]
+
+print(empleado)
+
+empleado.append((1, 1, 2016))
+
+print(empleado)
+
+alumno=("pedro",[7, 9] , "julian",[7, 9])
+
+print(alumno)
+
+alumno[1].append(10)
+
+print(alumno)
+
+
+
+candidatos=[ ("juan",[("cordoba",100),("buenos aires",200)]) , ("ana", [("cordoba",55)]) , ("luis", [("buenos aires",20)]) ]
+
+print(candidatos[(2)])
+
+print(candidatos.juan(1))
